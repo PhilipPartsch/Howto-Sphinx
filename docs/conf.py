d99cda1afc13bd4d643557628a7d6fce41609bcd
Philip Partsch <philip.partsch@posteo.de>
2024-08-28 11:20:31 +0000

    Update file conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -205,10 +205,43 @@ class NeedExampleDirective(SphinxDirective):
         self.state.nested_parse(self.content, self.content_offset, parsed)
         return [root]
 
-from sphinx_needs.logging import get_logger, log_warning
+from docutils.nodes import Node
+from sphinx import version_info
+from sphinx.util import logging
+from sphinx.util.logging import SphinxLoggerAdapter
 
-print('__name__')
-print(__name__)
+
+def get_logger(name: str) -> SphinxLoggerAdapter:
+    return logging.getLogger(name)
+
+
+def log_warning(
+    logger: SphinxLoggerAdapter,
+    message: str,
+    subtype: str | None,
+    /,
+    location: str | tuple[str | None, int | None] | Node | None,
+    *,
+    color: str | None = None,
+    once: bool = False,
+) -> None:
+    # Since sphinx in v7.3, sphinx will show warning types if `show_warning_types=True` is set,
+    # and in v8.0 this was made the default.
+    if version_info < (8,):
+        if subtype:
+            message += f" [needs.{subtype}]"
+        else:
+            message += " [needs]"
+
+    logger.warning(
+        message,
+        type="needs",
+        subtype=subtype,
+        location=location,
+        color=color,
+        once=once,
+    )
+
 logger = get_logger(__name__)
 
 
