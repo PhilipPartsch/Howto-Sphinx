4d16d373e3be058c41783134a188548bf06a8dc8
Philip Partsch <philip.partsch@posteo.de>
2024-12-02 09:59:00 +0000

    connect to other event within process_Code2Option

--- docs/conf.py
+++ docs/conf.py
@@ -363,7 +363,8 @@ def setup(app):
     for func in metamodel.needs_functions:
         add_dynamic_function(app, func)
 
-    app.connect("needs-before-post-processing", Code2Option)
+    #app.connect("needs-before-post-processing", process_Code2Option)
+    app.connect("doctree-resolved", process_Code2Option, priority=99,)
 
     app.connect("build-finished", metamodel.my_process_warnings)
 
